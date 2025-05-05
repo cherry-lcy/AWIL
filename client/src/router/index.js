ca2eca7c14477b89fe22682e52d9816b7bd1d36c
laich <laicheukying.cherry@gmail.com>
2025-05-05 23:48:13 +0800

    update router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,29 +1,29 @@
-import {createBrowserRouter} from "react-router-dom";
 import Login from  "../pages/Login";
 import AuthRoute from "../components/AuthRoute";
 import Layout from "../pages/Layout";
 import Dashboard from "../pages/Dashboard";
 import Panel from "../pages/Panel";
+import { createHashRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element:<Login/>
-    },
-    {
-        path:"/home",
-        element:<AuthRoute><Layout/></AuthRoute>,
-        children:[
-            {
-                path:'edit',
-                element: <Panel/>
-            },
-            {
-                path:'view',
-                element: <Dashboard/>
-            }
-        ]
-    }
-])
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "/home",
+    element: <AuthRoute><Layout /></AuthRoute>,
+    children: [
+      {
+        path: "edit",
+        element: <Panel />
+      },
+      {
+        path: "view",
+        element: <Dashboard />
+      }
+    ]
+  }
+]);
 
 export default router;
